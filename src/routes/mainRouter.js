2228639aab6c7a145cbada53ce8d26328f922c8a
NoistNT <arielgnr23@gmail.com>
2023-09-01 12:13:02 -0300

    remove loadDB in mainRouter

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -6,8 +6,8 @@ const pokemonRouter = require('./pokemonRouter')
 const typeRouter = require('./typeRouter')
 
 // Loading database with Types data
-const loadDB = async () => await getTypesData()
-loadDB()
+// const loadDB = async () => await getTypesData()
+// loadDB()
 
 const mainRouter = Router()
 
