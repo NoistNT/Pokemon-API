45ff38a150d64cedbf3ac196758e980c02ad9389
NoistNT <arielgnr23@gmail.com>
2023-12-16 15:09:21 -0300

    Revert "Merge pull request #14 from NoistNT/develop"
    This reverts commit 39d07b89f597d993e3e371bba05c53f2e2c04f79, reversing
    changes made to 69cafa0741770c5dc034e9248a9d25eb532230a3.

--- dist/src/routes/typeRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// Importing controllers
-const { getTypes } = require('../controllers/type/typesController');
-const typeRouter = (0, express_1.Router)();
-// Router configuration
-typeRouter.get('/', getTypes);
-module.exports = typeRouter;
