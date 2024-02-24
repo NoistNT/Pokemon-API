020f61c435e0307d6b0b685f8a48791801dff652
NoistNT <arielgnr23@gmail.com>
2024-02-24 19:15:35 -0300

    Add zod schema for pokemon and type

--- src/schemas/pokemon.schema.ts
+++ src/schemas/pokemon.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+import { z } from 'zod';
+import { createTypeSchema } from './type.schema';
 
 @Schema({ timestamps: true })
 export class Pokemon {
@@ -7,7 +9,7 @@ export class Pokemon {
     required: true,
     unique: true,
     trim: true,
-    default: crypto.randomUUID(),
+    default: () => crypto.randomUUID(),
   })
   id!: string;
   @Prop({
@@ -39,6 +41,31 @@ export class Pokemon {
   weight!: number;
   @Prop({ required: true, ref: 'Type' })
   type!: mongoose.Types.ObjectId;
+}
+
+export const createPokemonSchema = z.object({
+  id: z
+    .string()
+    .trim()
+    .default(() => crypto.randomUUID()),
+  name: z.string().min(2).max(30).trim().toLowerCase(),
+  image: z
+    .string()
+    .trim()
+    .default(
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    ),
+  hp: z.number().int().min(1).max(999).default(1),
+  attack: z.number().int().min(1).max(999).default(1),
+  defense: z.number().int().min(1).max(999).default(1),
+  speed: z.number().int().min(1).max(999).default(1),
+  height: z.number().int().min(1).max(999).default(1),
+  weight: z.number().int().min(1).max(999).default(1),
+  type: z.array(createTypeSchema).min(2),
+});
+
+export interface PokemonApiResponse {
+  results: { name: string }[];
 }
 
 export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- src/schemas/type.schema.ts
+++ src/schemas/type.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { z } from 'zod';
 
 @Schema({ timestamps: true })
 export class Type {
@@ -10,11 +11,21 @@ export class Type {
     trim: true,
     index: true,
     lowercase: true,
-    maxlength: 30,
+    maxlength: 20,
   })
   name!: string;
   @Prop({ required: true, trim: true, lowercase: true })
   url!: string;
+}
+
+export const createTypeSchema = z.object({
+  id: z.number().int().positive(),
+  name: z.string().min(2).max(20).trim().toLowerCase(),
+  url: z.string().url().trim(),
+});
+
+export interface TypeApiResponse {
+  results: Type[];
 }
 
 export const TypeSchema = SchemaFactory.createForClass(Type);
