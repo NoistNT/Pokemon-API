7bfe3e864ebd26f728a21ae7e295cde7e6c5a07b
NoistNT <arielgnr23@gmail.com>
2024-02-25 22:58:34 -0300

    Fix lint in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const { PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (PORT) {
