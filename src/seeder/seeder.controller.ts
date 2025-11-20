d21bcde4adf424adae5a77c0d135e86c2f1cb31c
NoistNT <arielgnr23@gmail.com>
2025-11-20 21:10:57 +0000

    feat: Implement Broken Access Control for Seeder Endpoint
    Secures the `/seeder` endpoint by changing it to a POST request and
    implementing API key authentication.

--- src/seeder/seeder.controller.ts
+++ src/seeder/seeder.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Headers, ForbiddenException } from '@nestjs/common';
 import { SeedResponse } from './entities/seeder.entity';
 import { SeederService } from './seeder.service';
 
@@ -7,20 +7,24 @@ export class SeederController {
   constructor(private readonly seederService: SeederService) {}
 
   /**
-   * Endpoint to trigger the seeding process using a GET request.
+   * Endpoint to trigger the seeding process using a POST request.
    * This will populate the database with initial or test data.
    *
    * @remarks
-   * - Requires appropriate authorization to access.
+   * - Requires a valid 'x-api-key' header for authorization.
    * - Typically used during initial setup, testing, or data reset.
    * - Consider alternative seeding mechanisms like CLI commands or environment variables.
    *
+   * @param {string} apiKey - The API key from the 'x-api-key' header.
    * @returns {Promise<SeedResponse>} An object containing the status and message of the seeding process:
    * - status: 201 for success, other codes for failure.
    * - message: A human-readable message indicating the outcome of the seeding process.
+   * @throws {ForbiddenException} If the provided API key is invalid.
    */
-  @Get()
-  async seed(): Promise<SeedResponse> {
+  @Post()
+  async seed(@Headers('x-api-key') apiKey: string): Promise<SeedResponse> {
+    if (apiKey !== process.env.SEEDER_API_KEY) throw new ForbiddenException('Invalid API Key');
+
     await this.seederService.seed();
 
     return {
